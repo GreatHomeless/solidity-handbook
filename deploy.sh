130014af6bc7e078803546334483307db065be2b
Donghun An <iandonghun@gmail.com>
2022-02-22 22:19:01 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ cd docs/.vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git init
 git add -A
 git commit -m 'deploy'
 
@@ -19,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/GreatHomeless/solidity-handbook.git origin:gh-pages
+git push -f https://github.com/GreatHomeless/solidity-handbook.git master:gh-pages
 
 cd -
